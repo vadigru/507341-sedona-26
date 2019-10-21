09a87a39d76cc02e2d560db5fc6f2751bce3b6de
vadigru <37663160+vadigru@users.noreply.github.com>
2019-10-21 06:31:28 +0300

    Corrections according mentor's comments

--- js/popup.js
+++ js/popup.js
@@ -7,11 +7,24 @@
   var kidsCount = block.querySelector('input[name="input-kids"]');
   var inputs = block.querySelectorAll('input');
 
+  var mapContainer = document.querySelector('.map');
+  var mapStatic = mapContainer.querySelector('IMG');
+
+  var mapInteractive = document.createElement('iframe');
+
   var isStorageSupport = true;
   var storageAdults = '';
   var storageKids = '';
 
   block.classList.add('search__container--hide');
+
+  mapContainer.removeChild(mapStatic);
+  mapContainer.appendChild(mapInteractive);
+  mapInteractive.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d707478.5249824972!2d-111.95179305361327!3d34.53963496499407!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872da132f942b00d%3A0x5548c523fa6c8efd!2sSedona%2C%20AZ%2086336%2C%20USA!5e0!3m2!1sen!2slv!4v1569730942957!5m2!1sen!2slv';
+  mapInteractive.title = 'Местонахождение города седона';
+  mapInteractive.width = '1200';
+  mapInteractive.height = '594';
+  mapInteractive.style.border = '0';
 
   try {
     storageAdults = localStorage.getItem('adults');
@@ -59,9 +72,9 @@
         block.classList.remove('search__container--error');
         void block.offsetWidth;
         block.classList.add('search__container--error');
-        item.style.backgroundColor = 'rgba(255, 205, 210, 1)';
+        item.style.boxShadow = 'inset 0 0 0 2px rgba(255, 205, 210, 1)';
       } else {
-        item.style.backgroundColor = 'rgba(200, 230, 201, 1)';
+        item.style.boxShadow = 'inset 0 0 0 2px rgba(200, 230, 201, 1)';
       }
     });
     if (isStorageSupport) {
